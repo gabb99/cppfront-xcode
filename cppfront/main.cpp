6c2f4610bc4996efc6f3019f59c91a0b4afa448e
Gabriel Beauchemin <gbeauchemin@hme.com>
2022-11-11 08:18:35 -0500

    Initial Commit

---
+++ cppfront/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cppfront
+//
+//  Created by Gabriel Beauchemin on 11/11/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
